251ed41cf21de8b16918e179a274f154202673a8
robertnegre <100676642+robertnegre@users.noreply.github.com>
2023-03-30 16:57:58 +0300

    robert a modificat main ul pe branch ul sau

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,26 +1,7 @@
-package src.sample;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
 
-public class Main extends Application { // comment nou:)
-
+public class Main {
     public static void main(String[] args) {
-        launch(args);
+        System.out.println("Fac ce vr pe branch ul meu!");
     }
-
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        /*
-        Parent root = FXMLLoader.load(getClass().getResource("fxml/view-students.fxml"));
-        primaryStage.setTitle("Student Manager");
-        primaryStage.setScene(new Scene(root, 600, 400));
-        primaryStage.show();
-
-         */
-    }
-
 }
