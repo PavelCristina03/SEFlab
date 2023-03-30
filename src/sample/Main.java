4d1c0380190ed57270a9d2338ac5c452d382dd3f
Gavril <donetlucky242@gmail.com>
2023-03-30 16:57:53 +0300

    doamne fereste ready to die

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -14,10 +14,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("fxml/view-students.fxml"));
-        primaryStage.setTitle("Student Manager");
-        primaryStage.setScene(new Scene(root, 600, 400));
-        primaryStage.show();
+        System.out.println("lnaooooooo hahahahahhahahahah hihihihih");
         //haha!
     }
 
