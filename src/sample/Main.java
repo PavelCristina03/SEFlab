1a96d567e1af7e850309f173dc8af974cbd5eaf4
Gavril <donetlucky242@gmail.com>
2023-03-30 16:41:29 +0300

    doamne fereste am scris un sout

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -14,10 +14,11 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("fxml/view-students.fxml"));
-        primaryStage.setTitle("Student Manager");
-        primaryStage.setScene(new Scene(root, 600, 400));
-        primaryStage.show();
+        //Parent root = FXMLLoader.load(getClass().getResource("fxml/view-students.fxml"));
+        //primaryStage.setTitle("Student Manager");
+        //primaryStage.setScene(new Scene(root, 600, 400));
+        //primaryStage.show();
+        System.out.println("ZUBI ZUBI ZUUUUUUUUUUUUUUUUUUUUUUUU");
     }
 
     //ana are mere faine
